9772a1c7ba22ba07439cb4c16ba3509f37a2001d
naineelshah <naineelshah@gmail.com>
2016-01-30 15:56:06 -0500

    Modified server.js and modified experiments index.html

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var app = express();
-app.use(express.static(_dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
+app.get('/sayHello', rootRequest);
+
+function rootRequest(req, res){
+    res.send('hello world');
+}
+
 app.listen(port, ipaddress);
