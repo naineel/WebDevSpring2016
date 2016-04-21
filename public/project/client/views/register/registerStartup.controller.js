3e9f472e520052311501bd7de9f7c1893c09c0b8
naineelshah <naineelshah@gmail.com>
2016-04-21 06:34:06 -0400

    Added register validations, home view links open in new tabs

--- public/project/client/views/register/registerStartup.controller.js
+++ public/project/client/views/register/registerStartup.controller.js
@@ -17,6 +17,33 @@
         init();
 
         function registerStartup(startup) {
+            vm.message = null;
+            if (startup == null) {
+                vm.message = "Please fill username, website and the high concept fields.";
+                return;
+            }
+
+            if (!startup.username) {
+                vm.message = "Please provide a username";
+                return;
+            }
+
+            if (!startup.username ||
+                    !startup.startupDetails.company_url ||
+                    !startup.startupDetails.high_concept) {
+                vm.message = "Please fill username, website and the high concept fields.";
+                return;
+            }
+
+            if (!startup.password || !startup.verifyPassword) {
+                vm.message = "Please provide a password";
+                return;
+            }
+
+            if (startup.password != startup.verifyPassword) {
+                vm.message = "Passwords must match";
+                return;
+            }
             startup.type = 'startup';
             //startup.set('type', 'startup');
             //startup.set('name', startup.username);
--- public/project/client/views/register/registerUser.controller.js
+++ public/project/client/views/register/registerUser.controller.js
@@ -15,6 +15,34 @@
         init();
 
         function registerUser(user) {
+            vm.message = null;
+            if (user == null) {
+                vm.message = "Please fill in the required fields";
+                return;
+            }
+
+            if (!user.username) {
+                vm.message = "Please provide a username";
+                return;
+            }
+
+            if (!user.username ||
+                    !user.userDetails.firstName ||
+                    !user.userDetails.lastName ||
+                    !user.userDetails.email) {
+                vm.message = "All fields are required!";
+                return;
+            }
+
+            if (!user.password || !user.password2) {
+                vm.message = "Please provide a password";
+                return;
+            }
+
+            if (user.password != user.password2) {
+                vm.message = "Passwords must match";
+                return;
+            }
             user.type = 'user';
             UserService
                 .registerUser(user)
